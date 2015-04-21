c3fd0856f2779a6fdd9430e77078a1fcf8f53218
David Edwards <david.l.edwards@gmail.com>
2015-04-21 14:27:42 -0500

    reorganize instruction codes; clean up optimizer

--- rpn-compiler/src/main/scala/com/loopfor/rpn/compiler/Code.scala
+++ rpn-compiler/src/main/scala/com/loopfor/rpn/compiler/Code.scala
@@ -8,7 +8,13 @@ abstract class BasicCode(op: String) extends Code(op) {
   val repr = s"$op"
 }
 
-abstract class ScalarCode(op: String, val args: Int) extends Code(op) {
+abstract class ScalarCode(op: String, val args: Int) extends Code(op)
+
+abstract class FixedScalarCode(op: String, args: Int) extends ScalarCode(op, args) {
+  val repr = s"$op"
+}
+
+abstract class DynamicScalarCode(op: String, args: Int) extends ScalarCode(op, args) {
   val repr = s"$op $args"
 }
 
@@ -24,14 +30,30 @@ case class PushCode(value: Double) extends Code("push") {
   val repr = s"$op $value"
 }
 
-case class AddCode(override val args: Int) extends ScalarCode("add", args)
-case class SubtractCode(override val args: Int) extends ScalarCode("sub", args)
-case class MultiplyCode(override val args: Int) extends ScalarCode("mul", args)
-case class DivideCode(override val args: Int) extends ScalarCode("div", args)
-case class MinCode(override val args: Int) extends ScalarCode("min", args)
-case class MaxCode(override val args: Int) extends ScalarCode("max", args)
+case class AddCode(override val args: Int) extends DynamicScalarCode("add", args)
+case class SubtractCode(override val args: Int) extends DynamicScalarCode("sub", args)
+case class MultiplyCode(override val args: Int) extends DynamicScalarCode("mul", args)
+case class DivideCode(override val args: Int) extends DynamicScalarCode("div", args)
+case class MinCode(override val args: Int) extends DynamicScalarCode("min", args)
+case class MaxCode(override val args: Int) extends DynamicScalarCode("max", args)
 
-case object ModuloCode extends BasicCode("mod")
-case object PowerCode extends BasicCode("pow")
-case object RootCode extends BasicCode("root")
+case object ModuloCode extends FixedScalarCode("mod", 2)
+case object PowerCode extends FixedScalarCode("pow", 2)
+case object RootCode extends FixedScalarCode("root", 2)
+
 case object NopCode extends BasicCode("nop")
+
+object Codes {
+  def format(codes: Seq[Code]): String = {
+    val (_, _, out) = ((0, 0, "") /: codes) { case ((pos, frame, out), c) =>
+      val f = c match {
+        case PushSymbolCode(_) => 1
+        case PushCode(_) => 1
+        case c: ScalarCode => 1 - c.args
+        case _ => 0
+      }
+      (pos + 1, frame + f, out + s"$pos [$frame] ${c.repr}\n")
+    }
+    out
+  }
+}
--- rpn-compiler/src/main/scala/com/loopfor/rpn/compiler/Optimizer.scala
+++ rpn-compiler/src/main/scala/com/loopfor/rpn/compiler/Optimizer.scala
@@ -4,19 +4,18 @@ import scala.annotation.tailrec
 
 class Optimizer private () {
   def apply(codes: Seq[Code]): Seq[Code] = {
-    simulate(codes)
-
-    @tailrec def loop(codes: Seq[Code]): Seq[Code] = {
-      val r = collapse(aggregate(codes))
-      val q = precompute(r)
-      // need better way to detect revisions other than comparing size of both sequences
-      if (q.size < r.size) loop(q)
-      else q
+    @tailrec def optimize(codes: Seq[Code]): Seq[Code] = {
+      val to = transform(codes)
+      if (to.size < codes.size) optimize(to) else to
     }
-    loop(codes)
+    println("---original code---")
+    println(Codes.format(codes))
+    optimize(codes)
   }
 
-  private val scalarCtors: Map[Class[_ <: ScalarCode], Int => ScalarCode] = Map(
+  private val transform = (flattenDynamicScalars _) andThen (combineDynamicScalars _) andThen (evaluateLiteralExpressions _)
+
+  private val dynamicCtors: Map[Class[_ <: DynamicScalarCode], Int => DynamicScalarCode] = Map(
         classOf[AddCode] -> AddCode.apply _,
         classOf[SubtractCode] -> SubtractCode.apply _,
         classOf[MultiplyCode] -> MultiplyCode.apply _,
@@ -25,66 +24,13 @@ class Optimizer private () {
         classOf[MaxCode] -> MaxCode.apply _
         )
 
-  private def aggregate(codes: Seq[Code]): Seq[Code] = {
-    val (_, _, _, revs) = codes.foldLeft((0, 0, Map.empty[(String, Int), (Int, ScalarCode)], Map.empty[Int, Code])) {
-      case ((pos, frame, candidates, revisions), code) =>
-        lazy val eligible = candidates filter { case ((_, f), _) => f <= frame }
+  private val scalarCtors: Map[Class[_ <: Code], Int => Code] = Map(
+        ModuloCode.getClass -> { _: Int => ModuloCode },
+        PowerCode.getClass -> { _: Int => PowerCode },
+        RootCode.getClass -> { _: Int => RootCode }
+        ) ++ dynamicCtors
 
-        def inspect(code: ScalarCode, ctor: Int => ScalarCode) = {
-          (eligible get (code.op, frame)) match {
-            case Some((p, c)) =>
-              val replace = ctor(c.args + code.args - 1)
-              (eligible - ((code.op, frame)) + ((replace.op, frame) -> (pos, replace)),
-                    revisions + (p -> NopCode) + (pos -> replace))
-            case None =>
-              (eligible + ((code.op, frame) -> (pos, code)), revisions)
-          }
-        }
-
-        val (f, c, r) = code match {
-          case PushSymbolCode(_) =>
-            (1, candidates, revisions)
-          case PushCode(_) =>
-            (1, candidates, revisions)
-          case ModuloCode | PowerCode | RootCode =>
-            (-1, eligible, revisions)
-          case _code: ScalarCode =>
-            val (_cands, _revs) = inspect(_code, scalarCtors(_code.getClass))
-            (1 - _code.args, _cands, _revs)
-          case _ =>
-            (0, candidates, revisions)
-        }
-
-        (pos + 1, frame + f, c, r)
-    }
-
-    println("---aggregate revisions---")
-    revs.toSeq.sortBy { case (k, _) => k } foreach { case (pos, code) => println(s"$pos: $code") }
-    revise(codes, revs)
-  }
-
-  private def collapse(codes: Seq[Code]): Seq[Code] = {
-    val (_, _, revs) = codes.foldLeft((0, Option.empty[ScalarCode], Map.empty[Int, Code])) { case ((pos, prior, revisions), code) =>
-      val (_prior, _revisions) = code match {
-        case c: ScalarCode =>
-          prior match {
-            case Some(p) if (c.op == p.op) =>
-              val replace = scalarCtors(c.getClass)(c.args + p.args - 1)
-              (Some(replace), revisions + (pos - 1 -> NopCode) + (pos -> replace))
-            case _ =>
-              (Some(c), revisions)
-          }
-        case _ =>
-          (None, revisions)
-      }
-      (pos + 1, _prior, _revisions)
-    }
-    println("---collapse revisions---")
-    revs.toSeq.sortBy { case (k, _) => k } foreach { case (pos, code) => println(s"$pos: $code") }
-    revise(codes, revs)
-  }
-
-  private val ops: Map[Class[_ <: Code], (Double, Double) => Double] = Map(
+  private val scalarOps: Map[Class[_ <: Code], (Double, Double) => Double] = Map(
         classOf[AddCode] -> { _ + _ },
         classOf[SubtractCode] -> { _ - _ },
         classOf[MultiplyCode] -> { _ * _ },
@@ -96,134 +42,124 @@ class Optimizer private () {
         RootCode.getClass -> { (n, exp) => Math.pow(n, 1 / exp) }
         )
 
-  private val opCtors: Map[Class[_ <: Code], Int => Code] = Map(
-        ModuloCode.getClass -> { _: Int => ModuloCode },
-        PowerCode.getClass -> { _: Int => PowerCode },
-        RootCode.getClass -> { _: Int => RootCode }
-        ) ++ scalarCtors
+  private def flattenDynamicScalars(codes: Seq[Code]): Seq[Code] = {
+    val (_, _, _, revs) = ((0, 0, Map.empty[(String, Int), (Int, DynamicScalarCode)], Map.empty[Int, Code]) /: codes) {
+      case ((pos, frame, cands, revs), code) =>
+        lazy val eligible = cands filter { case ((_, f), _) => f <= frame }
 
-  private def precompute(codes: Seq[Code]): Seq[Code] = {
+        def evaluate(code: DynamicScalarCode) = (eligible get (code.op, frame)) match {
+          case Some((p, c)) =>
+            val rep = dynamicCtors(code.getClass)(c.args + code.args - 1)
+            (eligible - ((code.op, frame)) + ((rep.op, frame) -> (pos, rep)),
+                  revs + (p -> NopCode) + (pos -> rep))
+          case None =>
+            (eligible + ((code.op, frame) -> (pos, code)), revs)
+        }
+
+        val (f, c, r) = code match {
+          case PushSymbolCode(_) => (1, cands, revs)
+          case PushCode(_) => (1, cands, revs)
+          case c: DynamicScalarCode =>
+            val (_cands, _revs) = evaluate(c)
+            (1 - c.args, _cands, _revs)
+          case c: ScalarCode => (1 - c.args, eligible, revs)
+          case _ => (0, cands, revs)
+        }
+        (pos + 1, frame + f, c, r)
+    }
+    println("---flatten revisions---")
+    printRevisions(revs)
+    revise(codes, revs)
+  }
+
+  private def combineDynamicScalars(codes: Seq[Code]): Seq[Code] = {
+    val (_, _, revs) = ((0, Option.empty[DynamicScalarCode], Map.empty[Int, Code]) /: codes) {
+      case ((pos, prior, revs), code) =>
+        val (p, r) = code match {
+          case c: DynamicScalarCode =>
+            prior match {
+              case Some(p) if (c.op == p.op) =>
+                val rep = dynamicCtors(c.getClass)(c.args + p.args - 1)
+                (Some(rep), revs + (pos - 1 -> NopCode) + (pos -> rep))
+              case _ => (Some(c), revs)
+            }
+          case _ => (None, revs)
+        }
+        (pos + 1, p, r)
+    }
+    println("---combine revisions---")
+    printRevisions(revs)
+    revise(codes, revs)
+  }
+
+  private def evaluateLiteralExpressions(codes: Seq[Code]): Seq[Code] = {
     trait Value
     case class NumberValue(value: Double, pos: Int) extends Value
     case object ArbitraryValue extends Value
 
     def analyze(pos: Int, codes: Seq[Code], stack: Seq[Value]): Map[Int, Code] = {
-
-      def inspect(op: Class[_ <: Code], n: Int): (Map[Int, Code], Seq[Value]) = {
-        val nums = for (arg @ NumberValue(_, _) <- (stack take n).reverse) yield arg
+      def inspect(code: ScalarCode): (Map[Int, Code], Seq[Value]) = {
+        val nums = for (arg @ NumberValue(_, _) <- (stack take code.args).reverse) yield arg
         val revs = if (nums.size > 1) {
           // apply the operation to each number in a left-to-right manner
-          val total = (for (NumberValue(v, _) <- nums) yield v) reduceLeft ops(op)
+          val total = (for (NumberValue(v, _) <- nums) yield v) reduceLeft scalarOps(code.getClass)
           // generate nop for all but the last push and modify last push with total
           val revs = nums.init.foldLeft(Map.empty[Int, Code]) { case (r, num) => r + (num.pos -> NopCode) } +
             (nums.last.pos -> PushCode(total))
-          if (nums.size == n) {
+          if (nums.size == code.args) {
             // when all arguments are numbers, eliminate the current op with a nop
             // since it is no longer needed
             revs + (pos -> NopCode)
           } else {
             // rewrite the op with a reduced number of arguments
-            revs + (pos -> opCtors(op)(n - nums.size + 1))
+            revs + (pos -> scalarCtors(code.getClass)(code.args - nums.size + 1))
           }
         } else
           Map.empty[Int, Code]
-        (revs, stack drop n)
+        (revs, stack drop code.args)
       }
 
       codes.headOption match {
-        case Some(DeclareSymbolCode(_)) =>
-          analyze(pos + 1, codes.tail, stack)
-        case Some(PushSymbolCode(_)) =>
-          analyze(pos + 1, codes.tail, ArbitraryValue +: stack)
-        case Some(PushCode(v)) =>
-          analyze(pos + 1, codes.tail, NumberValue(v, pos) +: stack)
+        case Some(DeclareSymbolCode(_)) => analyze(pos + 1, codes.tail, stack)
+        case Some(PushSymbolCode(_)) => analyze(pos + 1, codes.tail, ArbitraryValue +: stack)
+        case Some(PushCode(v)) => analyze(pos + 1, codes.tail, NumberValue(v, pos) +: stack)
         case Some(c: ScalarCode) =>
-          val (revs, st) = inspect(c.getClass, c.args)
-          if (revs.size == 0) analyze(pos + 1, codes.tail, st)
-          else revs
-        case Some(ModuloCode) =>
-          val (revs, st) = inspect(ModuloCode.getClass, 2)
-          if (revs.size == 0) analyze(pos + 1, codes.tail, st)
-          else revs
-        case Some(PowerCode) =>
-          val (revs, st) = inspect(PowerCode.getClass, 2)
-          if (revs.size == 0) analyze(pos + 1, codes.tail, st)
-          else revs
-        case Some(RootCode) =>
-          val (revs, st) = inspect(RootCode.getClass, 2)
-          if (revs.size == 0) analyze(pos + 1, codes.tail, st)
-          else revs
-        case None =>
-          Map.empty
-        case _ =>
-          analyze(pos + 1, codes.tail, stack)
+          val (revs, s) = inspect(c)
+          if (revs.size == 0) analyze(pos + 1, codes.tail, s) else revs
+        case None => Map.empty
+        case _ => analyze(pos + 1, codes.tail, stack)
       }
     }
 
-    @tailrec def loop(codes: Seq[Code]): Seq[Code] = {
+    @tailrec def evaluate(codes: Seq[Code]): Seq[Code] = {
       val revs = analyze(0, codes, Seq.empty)
       if (revs.isEmpty) codes
       else {
-        println("---precompute revisions---")
-        revs.toSeq.sortBy { case (k, _) => k } foreach { case (pos, code) => println(s"$pos: $code") }
-        loop(revise(codes, revs))
+        println("---evaluate revisions---")
+        printRevisions(revs)
+        evaluate(revise(codes, revs))
       }
     }
-
-    loop(codes)
+    evaluate(codes)
   }
 
   private def revise(codes: Seq[Code], revs: Map[Int, Code]): Seq[Code] = {
     if (revs.isEmpty)
       codes
     else {
-      val (_, _codes) = codes.foldLeft((0, Seq.empty[Code])) { case ((pos, revised), code) =>
+      val (_, cs) = codes.foldLeft((0, Seq.empty[Code])) { case ((pos, revised), code) =>
         (pos + 1, (revs get pos) match {
           case Some(NopCode) => revised
           case Some(c) => c +: revised
           case None => code +: revised
         })
       }
-      _codes.reverse
+      cs.reverse
     }
   }
 
-  private def simulate(codes: Seq[Code]): Unit = {
-    println("---simulation---")
-    ((0, 0) /: codes) { case ((p, l), c) =>
-      (p + 1, c match {
-        case PushSymbolCode(_) =>
-          println(s"$p: ${c.repr}")
-          l + 1
-        case PushCode(_) =>
-          println(s"$p: ${c.repr}")
-          l + 1
-        case AddCode(args) =>
-          println(s"$p: ${c.repr} --> $l")
-          l - args + 1
-        case SubtractCode(args) =>
-          println(s"$p: ${c.repr} --> $l")
-          l - args + 1
-        case MultiplyCode(args) =>
-          println(s"$p: ${c.repr} --> $l")
-          l - args + 1
-        case DivideCode(args) =>
-          println(s"$p: ${c.repr} --> $l")
-          l - args + 1
-        case MinCode(args) =>
-          println(s"$p: ${c.repr} --> $l")
-          l - args + 1
-        case MaxCode(args) =>
-          println(s"$p: ${c.repr} --> $l")
-          l - args + 1
-        case ModuloCode | PowerCode | RootCode =>
-          println(s"$p: ${c.repr}")
-          l - 1
-        case _ =>
-          println(s"$p: ${c.repr}")
-          l
-      })
-    }
+  private def printRevisions(revs: Map[Int, Code]): Unit = {
+    revs.toSeq.sortBy { case (p, _) => p } foreach { case (pos, code) => println(s"$pos: $code") }
   }
 }
 
